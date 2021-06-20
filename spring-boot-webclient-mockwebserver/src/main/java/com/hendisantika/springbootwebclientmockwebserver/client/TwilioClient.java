adfb05f573fe57a7521f7c63d032075f078c797b
Hendi Santika <hendisantika@gmail.com>
2021-06-20 16:49:23 +0700

    #29 - Add sendSms in TwilioClient  in spring-boot-webclient-mockwebserver

--- spring-boot-webclient-mockwebserver/src/main/java/com/hendisantika/springbootwebclientmockwebserver/client/TwilioClient.java
+++ spring-boot-webclient-mockwebserver/src/main/java/com/hendisantika/springbootwebclientmockwebserver/client/TwilioClient.java
@@ -20,4 +20,18 @@ public class TwilioClient {
     private final WebClient webClient;
     private final TwilioClientProperties properties;
 
+    public void sendSms(String from, String to, String message) {
+        String baseUrl = properties.getBaseUrl();
+        String accountSid = properties.getAccountSid();
+
+        TwilioMessageRequest request = new TwilioMessageRequest(to, from, message);
+
+        webClient.post()
+                .uri(baseUrl + "/Accounts/{AccountSid}/Messages.json", accountSid)
+                .bodyValue(request)
+                .retrieve()
+                .bodyToMono(TwilioMessageResponse.class)
+                .blockOptional()
+                .orElseThrow();
+    }
 }
