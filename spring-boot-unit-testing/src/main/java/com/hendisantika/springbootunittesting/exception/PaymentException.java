730a1894f88796526c62ecf30aada7ea4dd9ca2e
Hendi Santika <hendisantika@gmail.com>
2021-06-20 15:38:49 +0700

    #8 - Add PaymentException

---
+++ spring-boot-unit-testing/src/main/java/com/hendisantika/springbootunittesting/exception/PaymentException.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootunittesting.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/06/21
+ * Time: 15.38
+ */
+public class PaymentException extends RuntimeException {
+}
