3e1d6feaedcfba7f190bec8cd9e3c18cb73c709e
Hendi Santika <hendisantika@gmail.com>
2021-06-20 16:45:56 +0700

    #26 - Add getExchangeRate in ExchangeRateClient in spring-boot-webclient-mockwebserver

--- spring-boot-webclient-mockwebserver/src/main/java/com/hendisantika/springbootwebclientmockwebserver/client/ExchangeRateClient.java
+++ spring-boot-webclient-mockwebserver/src/main/java/com/hendisantika/springbootwebclientmockwebserver/client/ExchangeRateClient.java
@@ -1,9 +1,14 @@
 package com.hendisantika.springbootwebclientmockwebserver.client;
 
 import com.hendisantika.springbootwebclientmockwebserver.config.ExchangeClientProperties;
+import com.hendisantika.springbootwebclientmockwebserver.dto.ExchangeResponse;
+import com.hendisantika.springbootwebclientmockwebserver.exception.ExchangeFailure;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import javax.money.CurrencyUnit;
+import java.math.BigDecimal;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +24,23 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class ExchangeRateClient {
     private final WebClient webClient;
     private final ExchangeClientProperties properties;
+
+    public BigDecimal getExchangeRate(CurrencyUnit from, CurrencyUnit to) {
+        String baseUrl = properties.getBaseUrl();
+        String apiKey = properties.getApiKey();
+
+        return webClient.get()
+                .uri(baseUrl + "/v6/{apiKey}/pair/{from}/{to}", apiKey, from, to)
+                .retrieve()
+                .bodyToMono(ExchangeResponse.class)
+                .blockOptional()
+                .map(response -> {
+                    if ("error".equals(response.getResult())) {
+                        throw new ExchangeFailure();
+                    } else {
+                        return response.getConversionRate();
+                    }
+                })
+                .orElseThrow(ExchangeFailure::new);
+    }
 }
