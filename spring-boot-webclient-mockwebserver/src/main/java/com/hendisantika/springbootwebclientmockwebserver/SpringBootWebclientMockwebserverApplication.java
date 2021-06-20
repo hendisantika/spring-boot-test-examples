5a788cd3c2eefaa5ad4f89de7d12bee68b0e4e1a
Hendi Santika <hendisantika@gmail.com>
2021-06-20 15:52:17 +0700

    #13 - Add spring-boot-webclient-mockwebserver

---
+++ spring-boot-webclient-mockwebserver/src/main/java/com/hendisantika/springbootwebclientmockwebserver/SpringBootWebclientMockwebserverApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootwebclientmockwebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebclientMockwebserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebclientMockwebserverApplication.class, args);
+    }
+
+}
---
+++ spring-boot-webclient-mockwebserver/src/test/java/com/hendisantika/springbootwebclientmockwebserver/SpringBootWebclientMockwebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootwebclientmockwebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebclientMockwebserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
