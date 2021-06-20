5a8b6e910c34ffd5a3e2b8a05419c76887b651a2
Hendi Santika <hendisantika@gmail.com>
2021-06-20 16:55:21 +0700

    #35 - Add getOrder in OrderService in spring-boot-webclient-mockwebserver

--- spring-boot-webclient-mockwebserver/src/main/java/com/hendisantika/springbootwebclientmockwebserver/service/OrderService.java
+++ spring-boot-webclient-mockwebserver/src/main/java/com/hendisantika/springbootwebclientmockwebserver/service/OrderService.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.money.MonetaryAmount;
+import javax.persistence.EntityNotFoundException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -30,5 +31,9 @@ public class OrderService {
     public Order createOrder(MonetaryAmount amount) {
         Order order = new Order(LocalDateTime.now(), amount.getNumber().numberValue(BigDecimal.class), false);
         return orderRepository.save(order);
+    }
+
+    public Order getOrder(Long orderId) {
+        return orderRepository.findById(orderId).orElseThrow(EntityNotFoundException::new);
     }
 }
