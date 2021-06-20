c7c63d874e6345cde8385cfa5207141895579282
Hendi Santika <hendisantika@gmail.com>
2021-06-20 16:32:32 +0700

    #15 - Add Order entity in spring-boot-webclient-mockwebserver

---
+++ spring-boot-webclient-mockwebserver/src/main/java/com/hendisantika/springbootwebclientmockwebserver/entity/Order.java
@@ -0,0 +1,48 @@
+package com.hendisantika.springbootwebclientmockwebserver.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/06/21
+ * Time: 16.31
+ */
+@Entity(name = "orders")
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NonNull
+    private LocalDateTime date;
+    @NonNull
+    private BigDecimal amount;
+    @NonNull
+    private Boolean paid;
+
+    public boolean isPaid() {
+        return paid;
+    }
+
+    public Order markPaid() {
+        paid = true;
+        return this;
+    }
+}
