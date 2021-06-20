38c6ea5c2a236cfe97b8acc1fdd42f0610d7f5cb
Hendi Santika <hendisantika@gmail.com>
2021-06-20 16:42:17 +0700

    #23 - Add ExchangeFailure in spring-boot-webclient-mockwebserver

---
+++ spring-boot-webclient-mockwebserver/src/main/java/com/hendisantika/springbootwebclientmockwebserver/exception/ExchangeFailure.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootwebclientmockwebserver.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/06/21
+ * Time: 16.41
+ */
+public class ExchangeFailure extends RuntimeException {
+}
