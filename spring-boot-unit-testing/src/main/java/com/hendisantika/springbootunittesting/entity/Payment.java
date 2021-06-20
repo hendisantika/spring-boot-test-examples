b5236f5134b871228465269214e385ce801845a7
Hendi Santika <hendisantika@gmail.com>
2021-06-20 15:37:02 +0700

    #5 - Add Payment

---
+++ spring-boot-unit-testing/src/main/java/com/hendisantika/springbootunittesting/entity/Payment.java
@@ -0,0 +1,35 @@
+package com.hendisantika.springbootunittesting.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/06/21
+ * Time: 15.36
+ */
+@Entity
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class Payment {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @OneToOne
+    @NonNull
+    private Order order;
+    @NonNull
+    private String creditCardNumber;
+}
