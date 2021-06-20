44d83efaa47133b6dd0a00ff179642a7ab9e51ed
Hendi Santika <hendisantika@gmail.com>
2021-06-20 16:40:19 +0700

    #21 - Add OrderAlreadyPaid in spring-boot-webclient-mockwebserver

---
+++ spring-boot-webclient-mockwebserver/src/main/java/com/hendisantika/springbootwebclientmockwebserver/exception/OrderAlreadyPaid.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootwebclientmockwebserver.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/06/21
+ * Time: 16.39
+ */
+public class OrderAlreadyPaid extends RuntimeException {
+}
