87390feb652eba9476ec5c9fb0d78a36543eb00a
Hendi Santika <hendisantika@gmail.com>
2021-06-20 15:41:46 +0700

    #11 - Add payOrder Test Case  in OrderServiceTest

--- spring-boot-unit-testing/src/test/java/com/hendisantika/springbootunittesting/service/OrderServiceTest.java
+++ spring-boot-unit-testing/src/test/java/com/hendisantika/springbootunittesting/service/OrderServiceTest.java
@@ -1,11 +1,21 @@
 package com.hendisantika.springbootunittesting.service;
 
+import com.hendisantika.springbootunittesting.entity.Order;
+import com.hendisantika.springbootunittesting.entity.Payment;
 import com.hendisantika.springbootunittesting.repository.OrderRepository;
 import com.hendisantika.springbootunittesting.repository.PaymentRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +35,15 @@ class OrderServiceTest {
     @InjectMocks
     private OrderService orderService;
 
+    @Test
+    void payOrder() {
+        Order order = new Order(1L, false);
+        when(orderRepository.findById(1L)).thenReturn(Optional.of(order));
+        when(paymentRepository.save(any())).then(returnsFirstArg());
+
+        Payment payment = orderService.pay(1L, "4532756279624064");
+
+        assertThat(payment.getOrder().isPaid()).isTrue();
+        assertThat(payment.getCreditCardNumber()).isEqualTo("4532756279624064");
+    }
 }
