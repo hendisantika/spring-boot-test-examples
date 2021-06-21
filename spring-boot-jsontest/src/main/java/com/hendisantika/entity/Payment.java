eed8eae1895350bb57b7c84eac3488cc475a75d5
Hendi Santika <hendisantika@gmail.com>
2021-06-22 06:49:54 +0700

    #83 - Add Payment Entity in spring-boot-jsontest

---
+++ spring-boot-jsontest/src/main/java/com/hendisantika/entity/Payment.java
@@ -0,0 +1,37 @@
+package com.hendisantika.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/06/21
+ * Time: 06.49
+ */
+@Entity
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @OneToOne
+    @NonNull
+    private Order order;
+    @NonNull
+    private String creditCardNumber;
+}
