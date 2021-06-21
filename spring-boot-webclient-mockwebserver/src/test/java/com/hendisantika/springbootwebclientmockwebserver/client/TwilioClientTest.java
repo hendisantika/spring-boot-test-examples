a4115686181b0594512be987eae561dff1e5f655
Hendi Santika <hendisantika@gmail.com>
2021-06-21 08:17:33 +0700

    #50 - Add serializesRequest Test Case in TwilioClientTest in spring-boot-webclient-mockwebserver

--- spring-boot-webclient-mockwebserver/src/test/java/com/hendisantika/springbootwebclientmockwebserver/client/TwilioClientTest.java
+++ spring-boot-webclient-mockwebserver/src/test/java/com/hendisantika/springbootwebclientmockwebserver/client/TwilioClientTest.java
@@ -7,6 +7,7 @@ import okhttp3.mockwebserver.RecordedRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.json.BasicJsonTester;
+import org.springframework.boot.test.json.JsonContent;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -52,6 +53,24 @@ class TwilioClientTest {
 
         assertThat(request.getMethod()).isEqualTo("POST");
         assertThat(request.getPath()).isEqualTo("/Accounts/ACd936ed6dc1504dd79530f19f57b9c008/Messages.json");
+    }
+
+    @Test
+    void serializesRequest() throws InterruptedException {
+        mockWebServer.enqueue(
+                new MockResponse().setResponseCode(200)
+                        .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .setBody(getJson("message-response.json"))
+        );
+
+        twilioClient.sendSms("+123456", "+234567", "test message");
+
+        RecordedRequest request = mockWebServer.takeRequest();
+        JsonContent<Object> body = json.from(request.getBody().readUtf8());
+
+        assertThat(body).extractingJsonPathStringValue("$.from").isEqualTo("+123456");
+        assertThat(body).extractingJsonPathStringValue("$.to").isEqualTo("+234567");
+        assertThat(body).extractingJsonPathStringValue("$.body").isEqualTo("test message");
     }
 
 }
