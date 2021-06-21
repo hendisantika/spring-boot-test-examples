bc9d50964a00589b3c530e0b7ae51b7c53db6e58
Hendi Santika <hendisantika@gmail.com>
2021-06-21 08:18:58 +0700

    #53 - Add mockWebClient in TwilioClientUnitTests in spring-boot-webclient-mockwebserver

--- spring-boot-webclient-mockwebserver/src/test/java/com/hendisantika/springbootwebclientmockwebserver/client/TwilioClientUnitTests.java
+++ spring-boot-webclient-mockwebserver/src/test/java/com/hendisantika/springbootwebclientmockwebserver/client/TwilioClientUnitTests.java
@@ -1,6 +1,9 @@
 package com.hendisantika.springbootwebclientmockwebserver.client;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,4 +20,13 @@ public class TwilioClientUnitTests {
     private WebClient.RequestBodySpec requestBodyMock;
     private WebClient.ResponseSpec responseMock;
     private WebClient webClientMock;
+
+    @BeforeEach
+    void mockWebClient() {
+        requestBodyUriMock = mock(WebClient.RequestBodyUriSpec.class);
+        requestHeadersMock = mock(WebClient.RequestHeadersSpec.class);
+        requestBodyMock = mock(WebClient.RequestBodySpec.class);
+        responseMock = mock(WebClient.ResponseSpec.class);
+        webClientMock = mock(WebClient.class);
+    }
 }
