51b9130900391a8e9c952a66bce44befb1cf0ba7
Hendi Santika <hendisantika@gmail.com>
2021-06-21 09:04:29 +0700

    #58 - Add Payment Entity in spring-boot-datajpatest

---
+++ spring-boot-datajpatest/src/main/java/com/hendisantika/springbootdatajpatest/entity/Payment.java
@@ -0,0 +1,40 @@
+package com.hendisantika.springbootdatajpatest.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/06/21
+ * Time: 09.03
+ */
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"order_id"}))
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @OneToOne
+    @NonNull
+    private Order order;
+    @NonNull
+    private String creditCardNumber;
+}
