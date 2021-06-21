46b92d591e5798e1e5f2f2da24749ff951cafec1
Hendi Santika <hendisantika@gmail.com>
2021-06-21 08:11:01 +0700

    #40 - Add setupMockWebServer in ExchangeRateClientTest in spring-boot-webclient-mockwebserver

--- spring-boot-webclient-mockwebserver/src/test/java/com/hendisantika/springbootwebclientmockwebserver/client/ExchangeRateClientTest.java
+++ spring-boot-webclient-mockwebserver/src/test/java/com/hendisantika/springbootwebclientmockwebserver/client/ExchangeRateClientTest.java
@@ -1,6 +1,9 @@
 package com.hendisantika.springbootwebclientmockwebserver.client;
 
+import com.hendisantika.springbootwebclientmockwebserver.config.ExchangeClientProperties;
 import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.web.reactive.function.client.WebClient;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,5 +18,14 @@ class ExchangeRateClientTest {
     private MockWebServer mockWebServer;
     private ExchangeRateClient exchangeRateClient;
 
+    @BeforeEach
+    void setupMockWebServer() {
+        mockWebServer = new MockWebServer();
 
+        ExchangeClientProperties properties = new ExchangeClientProperties();
+        properties.setBaseUrl(mockWebServer.url("/").url().toString());
+        properties.setApiKey("029db72cee377e4bfa1fa413");
+
+        exchangeRateClient = new ExchangeRateClient(WebClient.create(), properties);
+    }
 }
