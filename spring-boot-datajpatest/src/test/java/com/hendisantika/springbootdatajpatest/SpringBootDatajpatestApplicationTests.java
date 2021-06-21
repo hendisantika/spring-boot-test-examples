aabb0771a2435d3095a0145f97e1deb7011b9117
Hendi Santika <hendisantika@gmail.com>
2021-06-21 08:58:17 +0700

    #55 - Add spring-boot-datajpatest

---
+++ spring-boot-datajpatest/src/test/java/com/hendisantika/springbootdatajpatest/SpringBootDatajpatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootdatajpatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDatajpatestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
