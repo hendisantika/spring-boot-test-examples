1b69f2d69c3d60127cea7c3a77a22dc97096a18c
Hendi Santika <hendisantika@gmail.com>
2021-06-21 08:20:24 +0700

    #54 - Add sendSms in TwilioClientUnitTests in spring-boot-webclient-mockwebserver

--- spring-boot-webclient-mockwebserver/src/test/java/com/hendisantika/springbootwebclientmockwebserver/client/TwilioClientUnitTests.java
+++ spring-boot-webclient-mockwebserver/src/test/java/com/hendisantika/springbootwebclientmockwebserver/client/TwilioClientUnitTests.java
@@ -1,9 +1,17 @@
 package com.hendisantika.springbootwebclientmockwebserver.client;
 
+import com.hendisantika.springbootwebclientmockwebserver.config.TwilioClientProperties;
+import com.hendisantika.springbootwebclientmockwebserver.dto.TwilioMessageRequest;
+import com.hendisantika.springbootwebclientmockwebserver.dto.TwilioMessageResponse;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,5 +36,26 @@ public class TwilioClientUnitTests {
         requestBodyMock = mock(WebClient.RequestBodySpec.class);
         responseMock = mock(WebClient.ResponseSpec.class);
         webClientMock = mock(WebClient.class);
+    }
+
+    @Test
+    void sendSms() {
+        TwilioClientProperties properties = new TwilioClientProperties();
+        properties.setBaseUrl("http://localhost");
+        properties.setAccountSid("accountSid");
+
+        TwilioClient client = new TwilioClient(webClientMock, properties);
+
+        TwilioMessageResponse response = new TwilioMessageResponse();
+        TwilioMessageRequest request = new TwilioMessageRequest("5678", "1234", "message");
+        String expectedUri = "http://localhost/Accounts/{AccountSid}/Messages.json";
+
+        when(webClientMock.post()).thenReturn(requestBodyUriMock);
+        when(requestBodyUriMock.uri(eq(expectedUri), eq("accountSid"))).thenReturn(requestBodyMock);
+        when(requestBodyMock.bodyValue(eq(request))).thenReturn(requestHeadersMock);
+        when(requestHeadersMock.retrieve()).thenReturn(responseMock);
+        when(responseMock.bodyToMono(TwilioMessageResponse.class)).thenReturn(Mono.just(response));
+
+        assertDoesNotThrow(() -> client.sendSms("1234", "5678", "message"));
     }
 }
