510333db1e9a208223650cd7c535e77922167930
Hendi Santika <hendisantika@gmail.com>
2021-06-22 06:45:06 +0700

    #80 - Add spring-boot-jsontest

---
+++ spring-boot-jsontest/src/main/java/com/hendisantika/SpringBootJsontestApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJsontestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJsontestApplication.class, args);
+    }
+
+}
