ecdefc98edc62ab581e98c3e713ae6b7cb8cc3ca
Hendi Santika <hendisantika@gmail.com>
2021-06-21 08:14:56 +0700

    #45 - Add mockWebClient in ExchangeRateUnitTests Test Case in ExchangeRateClientTest in spring-boot-webclient-mockwebserver

--- spring-boot-webclient-mockwebserver/src/test/java/com/hendisantika/springbootwebclientmockwebserver/client/ExchangeRateUnitTests.java
+++ spring-boot-webclient-mockwebserver/src/test/java/com/hendisantika/springbootwebclientmockwebserver/client/ExchangeRateUnitTests.java
@@ -1,6 +1,9 @@
 package com.hendisantika.springbootwebclientmockwebserver.client;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,4 +19,12 @@ public class ExchangeRateUnitTests {
     private WebClient.RequestBodySpec requestBodyMock;
     private WebClient.ResponseSpec responseMock;
     private WebClient webClientMock;
+
+    @BeforeEach
+    void mockWebClient() {
+        requestHeadersUriMock = mock(WebClient.RequestHeadersUriSpec.class);
+        requestBodyMock = mock(WebClient.RequestBodySpec.class);
+        responseMock = mock(WebClient.ResponseSpec.class);
+        webClientMock = mock(WebClient.class);
+    }
 }
