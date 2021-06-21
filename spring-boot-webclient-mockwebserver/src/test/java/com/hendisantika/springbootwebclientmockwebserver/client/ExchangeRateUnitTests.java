327a9a6c7d74420fc712b3f63d7ff520d32f0bc9
Hendi Santika <hendisantika@gmail.com>
2021-06-21 08:14:32 +0700

    #44 - Add ExchangeRateUnitTests Test Case in ExchangeRateClientTest in spring-boot-webclient-mockwebserver

---
+++ spring-boot-webclient-mockwebserver/src/test/java/com/hendisantika/springbootwebclientmockwebserver/client/ExchangeRateUnitTests.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootwebclientmockwebserver.client;
+
+import org.springframework.web.reactive.function.client.WebClient;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/06/21
+ * Time: 08.14
+ */
+public class ExchangeRateUnitTests {
+    private WebClient.RequestHeadersUriSpec requestHeadersUriMock;
+    private WebClient.RequestBodySpec requestBodyMock;
+    private WebClient.ResponseSpec responseMock;
+    private WebClient webClientMock;
+}
