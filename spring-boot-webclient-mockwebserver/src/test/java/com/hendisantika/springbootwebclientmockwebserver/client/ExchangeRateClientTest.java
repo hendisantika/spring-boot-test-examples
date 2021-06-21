336486f264739a83e3b36cdbc159892ed763265e
Hendi Santika <hendisantika@gmail.com>
2021-06-21 08:11:51 +0700

    #42 - Add makesTheCorrectRequest Test Case in ExchangeRateClientTest in spring-boot-webclient-mockwebserver

--- spring-boot-webclient-mockwebserver/src/test/java/com/hendisantika/springbootwebclientmockwebserver/client/ExchangeRateClientTest.java
+++ spring-boot-webclient-mockwebserver/src/test/java/com/hendisantika/springbootwebclientmockwebserver/client/ExchangeRateClientTest.java
@@ -3,6 +3,7 @@ package com.hendisantika.springbootwebclientmockwebserver.client;
 import com.hendisantika.springbootwebclientmockwebserver.config.ExchangeClientProperties;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
@@ -55,5 +56,23 @@ class ExchangeRateClientTest {
         BigDecimal rate = exchangeRateClient.getExchangeRate(eur, usd);
 
         assertThat(rate.doubleValue()).isEqualTo(0.8412);
+    }
+
+    @Test
+    void makesTheCorrectRequest() throws InterruptedException {
+        String json = "{\"conversion_rate\": 0.8412}";
+
+        mockWebServer.enqueue(
+                new MockResponse().setResponseCode(200)
+                        .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .setBody(json)
+        );
+
+        exchangeRateClient.getExchangeRate(Monetary.getCurrency("EUR"), Monetary.getCurrency("GBP"));
+
+        RecordedRequest request = mockWebServer.takeRequest();
+
+        assertThat(request.getMethod()).isEqualTo("GET");
+        assertThat(request.getPath()).isEqualTo("/v6/029db72cee377e4bfa1fa413/pair/EUR/GBP");
     }
 }
