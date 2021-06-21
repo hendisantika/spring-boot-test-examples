f767093a2b9e9dbc5b36501d6cb5379f3b0a97b8
Hendi Santika <hendisantika@gmail.com>
2021-06-22 06:51:09 +0700

    #85 - Add OrderAlreadyPaid in spring-boot-jsontest

---
+++ spring-boot-jsontest/src/main/java/com/hendisantika/exception/OrderAlreadyPaid.java
@@ -0,0 +1,13 @@
+package com.hendisantika.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/06/21
+ * Time: 06.50
+ */
+public class OrderAlreadyPaid extends RuntimeException {
+}
