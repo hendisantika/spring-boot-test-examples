92dc35f3cf717819a32380ba5b5a9471b3d68741
Hendi Santika <hendisantika@gmail.com>
2021-06-21 08:17:09 +0700

    #49 - Add makesTheCorrectRequest Test Case in TwilioClientTest in spring-boot-webclient-mockwebserver

--- spring-boot-webclient-mockwebserver/src/test/java/com/hendisantika/springbootwebclientmockwebserver/client/TwilioClientTest.java
+++ spring-boot-webclient-mockwebserver/src/test/java/com/hendisantika/springbootwebclientmockwebserver/client/TwilioClientTest.java
@@ -1,10 +1,17 @@
 package com.hendisantika.springbootwebclientmockwebserver.client;
 
 import com.hendisantika.springbootwebclientmockwebserver.config.TwilioClientProperties;
+import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.json.BasicJsonTester;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,6 +36,22 @@ class TwilioClientTest {
         properties.setAccountSid("ACd936ed6dc1504dd79530f19f57b9c008");
 
         twilioClient = new TwilioClient(WebClient.create(), properties);
+    }
+
+    @Test
+    void makesTheCorrectRequest() throws InterruptedException {
+        mockWebServer.enqueue(
+                new MockResponse().setResponseCode(200)
+                        .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .setBody(getJson("message-response.json"))
+        );
+
+        twilioClient.sendSms("+123456", "+234567", "test message");
+
+        RecordedRequest request = mockWebServer.takeRequest();
+
+        assertThat(request.getMethod()).isEqualTo("POST");
+        assertThat(request.getPath()).isEqualTo("/Accounts/ACd936ed6dc1504dd79530f19f57b9c008/Messages.json");
     }
 
 }
