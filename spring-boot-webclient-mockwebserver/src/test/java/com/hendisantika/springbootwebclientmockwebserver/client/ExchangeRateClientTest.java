e7ffc3539aa1c921bab1205f2ff228d5c737eb8d
Hendi Santika <hendisantika@gmail.com>
2021-06-21 08:11:29 +0700

    #41 - Add exchangeCurrency Test Case in ExchangeRateClientTest in spring-boot-webclient-mockwebserver

--- spring-boot-webclient-mockwebserver/src/test/java/com/hendisantika/springbootwebclientmockwebserver/client/ExchangeRateClientTest.java
+++ spring-boot-webclient-mockwebserver/src/test/java/com/hendisantika/springbootwebclientmockwebserver/client/ExchangeRateClientTest.java
@@ -1,9 +1,19 @@
 package com.hendisantika.springbootwebclientmockwebserver.client;
 
 import com.hendisantika.springbootwebclientmockwebserver.config.ExchangeClientProperties;
+import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import javax.money.CurrencyUnit;
+import javax.money.Monetary;
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,5 +37,23 @@ class ExchangeRateClientTest {
         properties.setApiKey("029db72cee377e4bfa1fa413");
 
         exchangeRateClient = new ExchangeRateClient(WebClient.create(), properties);
+    }
+
+    @Test
+    void exchangeCurrency() {
+        String json = "{\"conversion_rate\": 0.8412}";
+
+        mockWebServer.enqueue(
+                new MockResponse().setResponseCode(200)
+                        .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .setBody(json)
+        );
+
+        CurrencyUnit eur = Monetary.getCurrency("EUR");
+        CurrencyUnit usd = Monetary.getCurrency("USD");
+
+        BigDecimal rate = exchangeRateClient.getExchangeRate(eur, usd);
+
+        assertThat(rate.doubleValue()).isEqualTo(0.8412);
     }
 }
