e2c577797491ece4ccc3d6a45925f1ab8c5d9be0
Hendi Santika <hendisantika@gmail.com>
2021-06-21 09:13:35 +0700

    #67 - Add pay in OrderService in spring-boot-datajpatest

--- spring-boot-datajpatest/src/main/java/com/hendisantika/springbootdatajpatest/service/OrderService.java
+++ spring-boot-datajpatest/src/main/java/com/hendisantika/springbootdatajpatest/service/OrderService.java
@@ -1,9 +1,14 @@
 package com.hendisantika.springbootdatajpatest.service;
 
+import com.hendisantika.springbootdatajpatest.entity.Order;
+import com.hendisantika.springbootdatajpatest.entity.Payment;
+import com.hendisantika.springbootdatajpatest.exception.OrderAlreadyPaid;
 import com.hendisantika.springbootdatajpatest.repository.OrderRepository;
 import com.hendisantika.springbootdatajpatest.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +24,15 @@ import org.springframework.stereotype.Service;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final PaymentRepository paymentRepository;
+
+    public Payment pay(Long orderId, String creditCardNumber) {
+        Order order = orderRepository.findById(orderId).orElseThrow(EntityNotFoundException::new);
+
+        if (order.isPaid()) {
+            throw new OrderAlreadyPaid();
+        }
+
+        orderRepository.save(order.markPaid());
+        return paymentRepository.save(new Payment(order, creditCardNumber));
+    }
 }
