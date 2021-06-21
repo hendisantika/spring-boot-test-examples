12888bea84d9fb5fcd1579159270952edf5a615a
Hendi Santika <hendisantika@gmail.com>
2021-06-21 09:21:42 +0700

    #73 - Add PaymentRepositoryTest in spring-boot-datajpatest

---
+++ spring-boot-datajpatest/src/test/java/com/hendisantika/springbootdatajpatest/repository/PaymentRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.hendisantika.springbootdatajpatest.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.TestPropertySource;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/06/21
+ * Time: 09.20
+ */
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:tc:postgresql:13.2-alpine://payment"
+})
+class PaymentRepositoryTest {
+    @Autowired
+    private PaymentRepository paymentRepository;
+    @Autowired
+    private TestEntityManager entityManager;
+}
