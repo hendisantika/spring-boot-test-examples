50a89d0854736e33839e1f4aa874818279a9264f
Hendi Santika <hendisantika@gmail.com>
2021-06-21 08:10:42 +0700

    #39 - Add ExchangeRateClientTest in spring-boot-webclient-mockwebserver

---
+++ spring-boot-webclient-mockwebserver/src/test/java/com/hendisantika/springbootwebclientmockwebserver/client/ExchangeRateClientTest.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootwebclientmockwebserver.client;
+
+import okhttp3.mockwebserver.MockWebServer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/06/21
+ * Time: 08.10
+ */
+class ExchangeRateClientTest {
+    private MockWebServer mockWebServer;
+    private ExchangeRateClient exchangeRateClient;
+
+
+}
