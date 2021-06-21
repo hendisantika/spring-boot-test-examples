340f5c16c6fbe156f0e81f7c9a86c13bcccf3aa6
Hendi Santika <hendisantika@gmail.com>
2021-06-22 06:59:01 +0700

    #97 - Add createOrder in OrderService in spring-boot-jsontest

--- spring-boot-jsontest/src/main/java/com/hendisantika/service/OrderService.java
+++ spring-boot-jsontest/src/main/java/com/hendisantika/service/OrderService.java
@@ -1,9 +1,14 @@
 package com.hendisantika.service;
 
+import com.hendisantika.entity.Order;
 import com.hendisantika.repository.OrderRepository;
 import com.hendisantika.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.money.MonetaryAmount;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +24,9 @@ import org.springframework.stereotype.Service;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final PaymentRepository paymentRepository;
+
+    public Order createOrder(MonetaryAmount amount) {
+        Order order = new Order(LocalDateTime.now(), amount.getNumber().numberValue(BigDecimal.class), false);
+        return orderRepository.save(order);
+    }
 }
