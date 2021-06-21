dc40b4d20143bc29afca1db4e9ab4c2cd08a9e85
Hendi Santika <hendisantika@gmail.com>
2021-06-22 06:55:07 +0700

    #91 - Add MonetaryAmountSerializer DTO in spring-boot-jsontest

--- spring-boot-jsontest/src/main/java/com/hendisantika/dto/MoneySerialization.java
+++ spring-boot-jsontest/src/main/java/com/hendisantika/dto/MoneySerialization.java
@@ -1,10 +1,15 @@
 package com.hendisantika.dto;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import org.springframework.boot.jackson.JsonComponent;
 import org.springframework.context.i18n.LocaleContextHolder;
 
+import javax.money.MonetaryAmount;
 import javax.money.format.MonetaryAmountFormat;
 import javax.money.format.MonetaryFormats;
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +29,19 @@ public class MoneySerialization {
         monetaryAmountFormat = MonetaryFormats.getAmountFormat(LocaleContextHolder.getLocale());
     }
 
+    static class MonetaryAmountSerializer extends StdSerializer<MonetaryAmount> {
+
+        public MonetaryAmountSerializer() {
+            super(MonetaryAmount.class);
+        }
+
+        @Override
+        public void serialize(
+                MonetaryAmount value,
+                JsonGenerator generator,
+                SerializerProvider provider) throws IOException {
+
+            generator.writeString(monetaryAmountFormat.format(value));
+        }
+    }
 }
