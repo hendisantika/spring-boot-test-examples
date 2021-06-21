533b9fc0c718c86e37f26581c3e28f367145dc8e
Hendi Santika <hendisantika@gmail.com>
2021-06-21 09:15:34 +0700

    #70 - Add pay in OrderController in spring-boot-datajpatest

--- spring-boot-datajpatest/src/main/java/com/hendisantika/springbootdatajpatest/controller/OrderController.java
+++ spring-boot-datajpatest/src/main/java/com/hendisantika/springbootdatajpatest/controller/OrderController.java
@@ -1,8 +1,19 @@
 package com.hendisantika.springbootdatajpatest.controller;
 
+import com.hendisantika.springbootdatajpatest.dto.PaymentRequest;
+import com.hendisantika.springbootdatajpatest.dto.PaymentResponse;
+import com.hendisantika.springbootdatajpatest.entity.Payment;
 import com.hendisantika.springbootdatajpatest.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,4 +28,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
+
+    @PostMapping("/order/{id}/payment")
+    public ResponseEntity<PaymentResponse> pay(
+            @PathVariable("id") Long orderId,
+            @RequestBody @Valid PaymentRequest paymentRequest,
+            UriComponentsBuilder uriComponentsBuilder) {
+
+        Payment payment = orderService.pay(orderId, paymentRequest.getCreditCardNumber());
+        URI location = uriComponentsBuilder.path("/order/{id}/receipt").buildAndExpand(orderId).toUri();
+        PaymentResponse response = new PaymentResponse(payment.getOrder().getId(), payment.getCreditCardNumber());
+        return ResponseEntity.created(location).body(response);
+    }
 }
