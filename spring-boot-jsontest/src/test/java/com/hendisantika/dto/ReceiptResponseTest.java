1e28e31214b85d62775c71a5c880e410c60a14c7
Hendi Santika <hendisantika@gmail.com>
2021-06-22 07:09:12 +0700

    #108 - Add ReceiptResponseTests in spring-boot-jsontest

---
+++ spring-boot-jsontest/src/test/java/com/hendisantika/dto/ReceiptResponseTest.java
@@ -0,0 +1,21 @@
+package com.hendisantika.dto;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/06/21
+ * Time: 07.08
+ */
+@JsonTest
+class ReceiptResponseTests {
+    @Autowired
+    private JacksonTester<ReceiptResponse> jacksonTester;
+
+}
