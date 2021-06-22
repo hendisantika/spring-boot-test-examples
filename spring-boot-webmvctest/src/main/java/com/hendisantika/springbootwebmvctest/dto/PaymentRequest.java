67e10d5d66a29ee737f901b71640788333222b69
Hendi Santika <hendisantika@gmail.com>
2021-06-23 06:06:17 +0700

    #121 - Add PaymentRequest DTO in spring-boot-webmvctest

---
+++ spring-boot-webmvctest/src/main/java/com/hendisantika/springbootwebmvctest/dto/PaymentRequest.java
@@ -0,0 +1,26 @@
+package com.hendisantika.springbootwebmvctest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 23/06/21
+ * Time: 06.05
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentRequest {
+    @NotNull
+    @CreditCardNumber
+    private String creditCardNumber;
+}
