ca2757144e0a78dc37a85cd75ff756ca8a45f6cc
Hendi Santika <hendisantika@gmail.com>
2021-06-23 06:18:30 +0700

    #142 - Add creditCardNumberMustNotBeNull Test Case in PaymentRequestTests in spring-boot-webmvctest

--- spring-boot-webmvctest/src/test/java/com/hendisantika/springbootwebmvctest/PaymentRequestTests.java
+++ spring-boot-webmvctest/src/test/java/com/hendisantika/springbootwebmvctest/PaymentRequestTests.java
@@ -1,7 +1,14 @@
 package com.hendisantika.springbootwebmvctest;
 
+import com.hendisantika.springbootwebmvctest.dto.PaymentRequest;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,5 +21,14 @@ import javax.validation.Validator;
  */
 public class PaymentRequestTests {
     private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+    @Test
+    void creditCardNumberMustNotBeNull() {
+        PaymentRequest request = new PaymentRequest(null);
+
+        Set<ConstraintViolation<PaymentRequest>> violations = validator.validate(request);
+
+        assertThat(violations).isNotEmpty();
+    }
 
 }
