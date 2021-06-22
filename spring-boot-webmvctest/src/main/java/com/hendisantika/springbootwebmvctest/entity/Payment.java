36aac2c57a210c106e740e6baf8c654550ea5bb3
Hendi Santika <hendisantika@gmail.com>
2021-06-23 06:05:11 +0700

    #119 - Add Payment Entity in spring-boot-webmvctest

---
+++ spring-boot-webmvctest/src/main/java/com/hendisantika/springbootwebmvctest/entity/Payment.java
@@ -0,0 +1,37 @@
+package com.hendisantika.springbootwebmvctest.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 23/06/21
+ * Time: 06.04
+ */
+@Entity
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @OneToOne
+    @NonNull
+    private Order order;
+    @NonNull
+    private String creditCardNumber;
+}
