3b9e0dcc80a9352c579ae1e645c82a01513309c3
Hendi Santika <hendisantika@gmail.com>
2021-06-22 07:02:34 +0700

    #105 - Add getReceipt API in OrderController in spring-boot-jsontest

--- spring-boot-jsontest/src/main/java/com/hendisantika/controller/OrderController.java
+++ spring-boot-jsontest/src/main/java/com/hendisantika/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.hendisantika.dto.PaymentRequest;
 import com.hendisantika.dto.PaymentResponse;
 import com.hendisantika.entity.Order;
 import com.hendisantika.entity.Payment;
+import com.hendisantika.entity.Receipt;
 import com.hendisantika.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -58,5 +59,11 @@ public class OrderController {
         URI location = uriComponentsBuilder.path("/order/{id}/receipt").buildAndExpand(orderId).toUri();
         PaymentResponse response = new PaymentResponse(payment.getOrder().getId(), payment.getCreditCardNumber());
         return ResponseEntity.created(location).body(response);
+    }
+
+    @GetMapping("/order/{id}/receipt")
+    public ResponseEntity<Receipt> getReceipt(@PathVariable("id") Long orderId) {
+        Receipt receipt = orderService.getReceipt(orderId);
+        return ResponseEntity.ok().body(receipt);
     }
 }
