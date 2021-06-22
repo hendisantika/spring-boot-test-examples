aca4c1f6780da3143172dc0b4fbb8624f7af59b1
Hendi Santika <hendisantika@gmail.com>
2021-06-22 07:11:18 +0700

    #109 - Add serializeInCorrectFormat in ReceiptResponseTests in spring-boot-jsontest

--- spring-boot-jsontest/src/test/java/com/hendisantika/dto/ReceiptResponseTest.java
+++ spring-boot-jsontest/src/test/java/com/hendisantika/dto/ReceiptResponseTest.java
@@ -1,8 +1,17 @@
 package com.hendisantika.dto;
 
+import org.javamoney.moneta.Money;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+
+import javax.money.Monetary;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,8 +23,20 @@ import org.springframework.boot.test.json.JacksonTester;
  * Time: 07.08
  */
 @JsonTest
-class ReceiptResponseTests {
+class ReceiptResponseTest {
     @Autowired
     private JacksonTester<ReceiptResponse> jacksonTester;
 
+    @Test
+    void serializeInCorrectFormat() throws IOException {
+        ReceiptResponse receipt = new ReceiptResponse(
+                LocalDateTime.of(2021, 5, 9, 16, 0),
+                "4532756279624064",
+                Money.of(50.0, Monetary.getCurrency("USD")));
+
+        JsonContent<ReceiptResponse> json = jacksonTester.write(receipt);
+
+        assertThat(json).extractingJsonPathStringValue("$.date").isEqualTo("09.05.2021 16:00");
+        assertThat(json).extractingJsonPathStringValue("$.amount").isEqualTo("USD50.00");
+    }
 }
