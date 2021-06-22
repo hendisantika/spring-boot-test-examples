53d3526b7e36e8f43f054ae44d43046563eb876a
Hendi Santika <hendisantika@gmail.com>
2021-06-22 07:21:08 +0700

    #115 - Add givenOrderIsPaid in OrderControllerTests in spring-boot-jsontest

--- spring-boot-jsontest/src/test/java/com/hendisantika/OrderControllerTests.java
+++ spring-boot-jsontest/src/test/java/com/hendisantika/OrderControllerTests.java
@@ -1,12 +1,22 @@
 package com.hendisantika;
 
 import com.hendisantika.controller.OrderController;
+import com.hendisantika.entity.Receipt;
 import com.hendisantika.service.OrderService;
+import org.javamoney.moneta.Money;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import javax.money.Monetary;
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 /**
  * Created by IntelliJ IDEA.
@@ -32,5 +42,13 @@ class OrderControllerTests {
                 .andExpect(jsonPath("$.date").isNotEmpty())
                 .andExpect(jsonPath("$.creditCardNumber").isNotEmpty())
                 .andExpect(jsonPath("$.amount").isNotEmpty());
+    }
+
+    private void givenOrderIsPaid(Long orderId) {
+        Receipt receipt = new Receipt(
+                LocalDateTime.now(),
+                "4532756279624064",
+                Money.of(100.0, Monetary.getCurrency("EUR")));
+        when(orderService.getReceipt(eq(orderId))).thenReturn(receipt);
     }
 }
