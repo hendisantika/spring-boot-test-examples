4942b6dd911e013a755823ed9704eb2e3b54d994
Hendi Santika <hendisantika@gmail.com>
2021-06-23 06:02:26 +0700

    #116 - Add spring-boot-webmvctest

---
+++ spring-boot-webmvctest/src/main/java/com/hendisantika/springbootwebmvctest/SpringBootWebmvctestApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootwebmvctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebmvctestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebmvctestApplication.class, args);
+    }
+
+}
