b2b44e2d6e1dfad28d5a8307487dfd23098441ac
Hendi Santika <hendisantika@gmail.com>
2021-06-24 08:03:26 +0700

    #191 - Add getReceipt Test Case in MockEnvIntegrationTests in spring-boot-integration-testing

--- spring-boot-integration-testing/src/test/java/com/hendisantika/springbootintegrationtesting/MockEnvIntegrationTests.java
+++ spring-boot-integration-testing/src/test/java/com/hendisantika/springbootintegrationtesting/MockEnvIntegrationTests.java
@@ -12,6 +12,12 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.money.CurrencyUnit;
+import javax.money.Monetary;
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -51,5 +57,17 @@ class MockEnvIntegrationTests {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\"creditCardNumber\": \"4532756279624064\"}"))
                 .andExpect(status().isCreated());
+    }
+
+    @Test
+    @Sql("/paid-order.sql")
+    void getReceipt() throws Exception {
+        CurrencyUnit eur = Monetary.getCurrency("EUR");
+        CurrencyUnit usd = Monetary.getCurrency("USD");
+
+        when(exchangeRateClient.getExchangeRate(eur, usd)).thenReturn(BigDecimal.valueOf(0.8412));
+
+        mockMvc.perform(get("/order/{id}/receipt?currency=USD", 1))
+                .andExpect(status().isOk());
     }
 }
