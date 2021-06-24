7d10abc94a40c92d12a137f4f61a924c682aef00
Hendi Santika <hendisantika@gmail.com>
2021-06-24 08:02:01 +0700

    #190 - Add payOrder Test Case in MockEnvIntegrationTests in spring-boot-integration-testing

--- spring-boot-integration-testing/src/test/java/com/hendisantika/springbootintegrationtesting/MockEnvIntegrationTests.java
+++ spring-boot-integration-testing/src/test/java/com/hendisantika/springbootintegrationtesting/MockEnvIntegrationTests.java
@@ -8,8 +8,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 /**
@@ -37,6 +41,15 @@ class MockEnvIntegrationTests {
         mockMvc.perform(post("/order")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\"amount\": \"EUR100.0\"}"))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    @Sql("/unpaid-order.sql")
+    void payOrder() throws Exception {
+        mockMvc.perform(post("/order/{id}/payment", 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"creditCardNumber\": \"4532756279624064\"}"))
                 .andExpect(status().isCreated());
     }
 }
