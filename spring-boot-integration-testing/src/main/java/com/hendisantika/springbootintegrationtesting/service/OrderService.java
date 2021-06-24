c829ac2b19976f866e11d38bea78eaa59a228b54
Hendi Santika <hendisantika@gmail.com>
2021-06-24 07:43:27 +0700

    #168 - Add createOrder in OrderService in spring-boot-integration-testing

--- spring-boot-integration-testing/src/main/java/com/hendisantika/springbootintegrationtesting/service/OrderService.java
+++ spring-boot-integration-testing/src/main/java/com/hendisantika/springbootintegrationtesting/service/OrderService.java
@@ -1,10 +1,15 @@
 package com.hendisantika.springbootintegrationtesting.service;
 
 import com.hendisantika.springbootintegrationtesting.client.ExchangeRateClient;
+import com.hendisantika.springbootintegrationtesting.entity.Order;
 import com.hendisantika.springbootintegrationtesting.repository.OrderRepository;
 import com.hendisantika.springbootintegrationtesting.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.money.MonetaryAmount;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +26,10 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final PaymentRepository paymentRepository;
     private final ExchangeRateClient exchangeRateClient;
+
+    public Order createOrder(MonetaryAmount amount) {
+        Order order = new Order(LocalDateTime.now(), amount.getNumber().numberValue(BigDecimal.class), false);
+        return orderRepository.save(order);
+    }
+
 }
--- spring-boot-integration-testing/src/main/java/com/hendisantika/springbootintegrationtesting/util/MoneySerialization.java
+++ spring-boot-integration-testing/src/main/java/com/hendisantika/springbootintegrationtesting/util/MoneySerialization.java
@@ -1,6 +1,7 @@
 package com.hendisantika.springbootintegrationtesting.util;
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.SerializerProvider;
