7ad0736a9cf49e054b46a53342ea7586e392b68d
Hendi Santika <hendisantika@gmail.com>
2021-06-24 08:00:00 +0700

    #188 - Add MockEnvIntegrationTests in spring-boot-integration-testing

---
+++ spring-boot-integration-testing/src/test/java/com/hendisantika/springbootintegrationtesting/MockEnvIntegrationTests.java
@@ -0,0 +1,32 @@
+package com.hendisantika.springbootintegrationtesting;
+
+import com.hendisantika.springbootintegrationtesting.client.ExchangeRateClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/06/21
+ * Time: 07.59
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@ActiveProfiles("test")
+class MockEnvIntegrationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ExchangeRateClient exchangeRateClient;
+}
