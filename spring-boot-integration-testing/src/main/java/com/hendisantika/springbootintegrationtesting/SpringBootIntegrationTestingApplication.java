c700ff2dbac91e5d8d10acea4e2cc505de175b27
Hendi Santika <hendisantika@gmail.com>
2021-06-24 07:13:53 +0700

    #144 - Add spring-boot-integration-testing

---
+++ spring-boot-integration-testing/src/main/java/com/hendisantika/springbootintegrationtesting/SpringBootIntegrationTestingApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootintegrationtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntegrationTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootIntegrationTestingApplication.class, args);
+    }
+
+}
