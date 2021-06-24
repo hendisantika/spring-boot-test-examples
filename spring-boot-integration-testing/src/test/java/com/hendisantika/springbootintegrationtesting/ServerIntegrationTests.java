0c058debe2aafe19715acdc6ad5c73c06ed7bed3
Hendi Santika <hendisantika@gmail.com>
2021-06-24 07:55:39 +0700

    #182 - Add registerProperties in ServerIntegrationTests in spring-boot-integration-testing

--- spring-boot-integration-testing/src/test/java/com/hendisantika/springbootintegrationtesting/ServerIntegrationTests.java
+++ spring-boot-integration-testing/src/test/java/com/hendisantika/springbootintegrationtesting/ServerIntegrationTests.java
@@ -6,6 +6,8 @@ import okhttp3.mockwebserver.MockWebServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 /**
@@ -27,4 +29,9 @@ class ServerIntegrationTests {
     private OrderRepository orderRepository;
     @Autowired
     private PaymentRepository paymentRepository;
+
+    @DynamicPropertySource
+    static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("exchange-rate-api.base-url", () -> mockWebServer.url("/").url().toString());
+    }
 }
