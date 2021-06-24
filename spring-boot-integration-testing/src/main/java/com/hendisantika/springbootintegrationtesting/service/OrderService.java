a6bfc11b30cd5600f4944d8e5d47f23e2e6001fc
Hendi Santika <hendisantika@gmail.com>
2021-06-24 07:32:14 +0700

    #158 - Add OrderService in spring-boot-integration-testing

---
+++ spring-boot-integration-testing/src/main/java/com/hendisantika/springbootintegrationtesting/service/OrderService.java
@@ -0,0 +1,23 @@
+package com.hendisantika.springbootintegrationtesting.service;
+
+import com.hendisantika.springbootintegrationtesting.repository.OrderRepository;
+import com.hendisantika.springbootintegrationtesting.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/06/21
+ * Time: 07.31
+ */
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final PaymentRepository paymentRepository;
+    private final ExchangeRateClient exchangeRateClient;
+}
