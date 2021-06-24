dbadb5ff7371d963838780a9ad08830a25e343fb
Hendi Santika <hendisantika@gmail.com>
2021-06-24 07:25:33 +0700

    #152 - Add OrderAlreadyPaid Exception in spring-boot-integration-testing

---
+++ spring-boot-integration-testing/src/main/java/com/hendisantika/springbootintegrationtesting/exception/OrderAlreadyPaid.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootintegrationtesting.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/06/21
+ * Time: 07.25
+ */
+public class OrderAlreadyPaid extends RuntimeException {
+}
