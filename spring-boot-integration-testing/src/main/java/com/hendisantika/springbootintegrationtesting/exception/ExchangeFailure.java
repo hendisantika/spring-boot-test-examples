5c2adabba4a2c3ab0b44f60864a49d6e0d0b3dda
Hendi Santika <hendisantika@gmail.com>
2021-06-24 07:37:55 +0700

    #162 - Add ExchangeFailure in spring-boot-integration-testing

---
+++ spring-boot-integration-testing/src/main/java/com/hendisantika/springbootintegrationtesting/exception/ExchangeFailure.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootintegrationtesting.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/06/21
+ * Time: 07.37
+ */
+public class ExchangeFailure extends RuntimeException {
+}
