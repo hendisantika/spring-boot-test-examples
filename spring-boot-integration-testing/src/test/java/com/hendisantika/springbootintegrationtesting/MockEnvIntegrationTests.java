b23b8e9adfe4eaf5f16e41cd42e15810e68daaa5
Hendi Santika <hendisantika@gmail.com>
2021-06-24 08:00:45 +0700

    #189 - Add createOrder Test Case in MockEnvIntegrationTests in spring-boot-integration-testing

--- spring-boot-integration-testing/src/test/java/com/hendisantika/springbootintegrationtesting/MockEnvIntegrationTests.java
+++ spring-boot-integration-testing/src/test/java/com/hendisantika/springbootintegrationtesting/MockEnvIntegrationTests.java
@@ -1,10 +1,12 @@
 package com.hendisantika.springbootintegrationtesting;
 
 import com.hendisantika.springbootintegrationtesting.client.ExchangeRateClient;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,4 +31,12 @@ class MockEnvIntegrationTests {
 
     @MockBean
     private ExchangeRateClient exchangeRateClient;
+
+    @Test
+    void createOrder() throws Exception {
+        mockMvc.perform(post("/order")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"amount\": \"EUR100.0\"}"))
+                .andExpect(status().isCreated());
+    }
 }
