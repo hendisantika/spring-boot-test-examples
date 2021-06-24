8bdffdaeb13abb4fac48b795464de45efa1a8468
Hendi Santika <hendisantika@gmail.com>
2021-06-24 07:38:26 +0700

    #163 - Add getExchangeRate in ExchangeRateClient in spring-boot-integration-testing

--- spring-boot-integration-testing/src/main/java/com/hendisantika/springbootintegrationtesting/client/ExchangeRateClient.java
+++ spring-boot-integration-testing/src/main/java/com/hendisantika/springbootintegrationtesting/client/ExchangeRateClient.java
@@ -1,8 +1,13 @@
 package com.hendisantika.springbootintegrationtesting.client;
 
+import com.hendisantika.springbootintegrationtesting.dto.ExchangeResponse;
+import com.hendisantika.springbootintegrationtesting.exception.ExchangeFailure;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import javax.money.CurrencyUnit;
+import java.math.BigDecimal;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +23,23 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class ExchangeRateClient {
     private final WebClient webClient;
     private final ExchangeClientProperties properties;
+
+    public BigDecimal getExchangeRate(CurrencyUnit from, CurrencyUnit to) {
+        String baseUrl = properties.getBaseUrl();
+        String apiKey = properties.getApiKey();
+
+        return webClient.get()
+                .uri(baseUrl + "/v6/{apiKey}/pair/{from}/{to}", apiKey, from, to)
+                .retrieve()
+                .bodyToMono(ExchangeResponse.class)
+                .blockOptional()
+                .map(response -> {
+                    if ("error".equals(response.getResult())) {
+                        throw new ExchangeFailure();
+                    } else {
+                        return response.getConversionRate();
+                    }
+                })
+                .orElseThrow(ExchangeFailure::new);
+    }
 }
